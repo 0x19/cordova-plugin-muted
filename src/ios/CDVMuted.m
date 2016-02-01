cfe4664378598781a2b4c2d82723bf6ecd9a1761
Nevio <nevio.vesic@gmail.com>
2016-02-01 17:16:34 +0100

    Adding more fixes to plugin / ios

--- src/ios/CDVMuted.m
+++ src/ios/CDVMuted.m
@@ -1,5 +1,8 @@
-#import "Echo.h"
+#import "CDVMuted.h"
 #import <Cordova/CDV.h>
+#import <MediaPlayer/MediaPlayer.h>
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioToolbox.h>
 
 #define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
 
@@ -10,6 +13,11 @@
 {
   if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0")) {
       float vol = [[AVAudioSession sharedInstance] outputVolume];
+
+      if(vol < 0.1) {
+          return YES;
+      }
+
       return NO;
   }
 
@@ -25,7 +33,7 @@
   if (volumeViewSlider) {
       float val = [volumeViewSlider value];
 
-      if(volumeViewSlider < 0.1) {
+      if(val < 0.1) {
         return YES;
       }
   }
